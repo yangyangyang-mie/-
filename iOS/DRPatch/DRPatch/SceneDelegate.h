914680f5de940de517ae80476ddc68b3561f74b0
羊羊羊 <>
2020-01-31 15:12:40 +0800

    先使用iOS13进行开发

---
+++ iOS/DRPatch/DRPatch/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  DRPatch
+//
+//  Created by yyy on 2020/1/31.
+//  Copyright © 2020 yyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
